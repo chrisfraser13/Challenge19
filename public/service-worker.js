6b8a5b12b0760c6666742408708a9bf0f4e5e85a
Chris Fraser <chrisfraser@Chriss-MBP.home>
2022-05-24 13:25:15 -0400

    gettting repo in order

---
+++ public/service-worker.js
@@ -0,0 +1,96 @@
+
+//Copied over from 04 of activities. Need to be edited to match this challenge
+const APP_PREFIX = "my-site-cache";
+const VERSION = "v1";
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/css/style.css',
+  '/js/idb.js',
+  '/js/index.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+];
+
+// Install the service worker
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Your files were pre-cached successfully!');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+});
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+        let cacheKeeplist = keyList.filter(function(key){
+            return key.indexOf(APP_PREFIX)
+        }) 
+        cacheKeeplist.push(CACHE_NAME)
+      return Promise.all(
+        keyList.map((key, i) => {
+          if (cacheKeeplist.indexOf(key)=== -1) {
+            console.log('Removing old cache data', key);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+
+});
+
+// Intercept fetch requests
+self.addEventListener('fetch', function(evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request).catch(function() {
+      return caches.match(evt.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get('accept').includes('text/html')) {
+          // return the cached home page for all requests for html pages
+          return caches.match('/');
+        }
+      });
+    })
+  );
+});
